9212b97db8bf08fd56095c982882ff7a1fb1b0e3
Niral Khambhati <f20170130@hyderabad.bits-hyderabad.ac.in>
2019-10-31 17:10:18 +0530

    Revert "UI: improvements"
    This reverts commit e98420ee48a5b13f909643f28ed7e8539c8f9e17.
    
    UI:Improvements revert

--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -3,38 +3,42 @@ import { Link } from "react-router-dom";
 import "rc-collapse/assets/index.css";
 import Container from "react-bootstrap/Container";
 import LazyHero from "react-lazy-hero";
+import { axiosGET } from "../utils/axiosClient";
 import { getDecodedToken } from "../utils/jwt";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 class StudentDashboard extends Component {
   constructor(props) {
     super(props);
     this.user = getDecodedToken();
+    console.log(this.user);
   }
   render() {
     return (
       <Container>
         <LazyHero>
           <div>
-            <h1>Welcome, {this.user.name}!</h1>
+            <h1>THIS IS THE STUDENT DASHBOARD!</h1>
           </div>
-          <br />
-          <br />
-          <br />
-          <Row>
-            <Col>
-              <Link to={`/booking`}>
-                <Button>Lab Bookings</Button>
-              </Link>
-            </Col>
-            <Col>
-              <Link to={`/installrequest`}>
-                <Button>Software Installation Requests</Button>
-              </Link>
-            </Col>
-          </Row>
+          <Card bg="secondary">
+            <Row>
+              <Col>
+                {" "}
+                <Link to={`/booking`}>
+                  <Button>Lab Bookings</Button>
+                </Link>
+              </Col>
+              <Col>
+                {" "}
+                <Link to={`/installrequest`}>
+                  <Button>Software Installation Requests</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Card>
         </LazyHero>
       </Container>
     );
