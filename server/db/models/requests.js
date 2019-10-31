d68a67d3e8856d023d217832e9084b254c36e575
Niral Khambhati <f20170130@hyderabad.bits-hyderabad.ac.in>
2019-10-31 17:19:22 +0530

    add Request schema to mongodb

---
+++ server/db/models/requests.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    index: true
+  },
+  status: {
+    type: String,
+    required: true
+  }, // ['Accepted', 'Rejected', 'Waiting', 'Requested']
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model("Request", requestSchema);
