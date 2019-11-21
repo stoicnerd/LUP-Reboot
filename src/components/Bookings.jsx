6770559dd0cf11d241c4831aedcf4c7586a0e54f
Sourav <sanganeria.sourav@gmail.com>
2019-11-21 16:00:37 +0530

    Events displayed

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -14,23 +14,6 @@ import moment from "moment";
 
 const localizer = momentLocalizer(moment);
 
-const MyCalendar = props => (
-  <div>
-    <Calendar
-      localizer={localizer}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500, width: 900 }}
-      events={[
-        {
-          title: "All Day Event very long title",
-          start: new Date("2019-11-25T00:00"),
-          end: new Date("2019-11-25T23:00")
-        }
-      ]}
-    />
-  </div>
-);
 class Bookings extends Component {
   constructor(props) {
     super(props);
@@ -58,20 +41,27 @@ class Bookings extends Component {
 
   async getEvents() {
     let allEvents = [];
-    await axiosGET(`http://localhost:4000/api/events/`).then(res => {
+    await axiosGET(`/api/events/`).then(res => {
       // console.log(res);
       // console.log("Hi");
       allEvents = res.data;
-      console.log(allEvents);
     });
-    this.setState({ events: allEvents });
+    let finalEvent = [];
+    allEvents.forEach(event => {
+      finalEvent.push({
+        title: event.description,
+        start: new Date(event.startTime),
+        end: new Date(event.endTime)
+      });
+    });
+    this.setState({ events: finalEvent });
   }
 
   generateRequestsList() {
     let allrequests = [];
     let temp = [];
     temp = this.state.requests;
-    console.log(this.state.requests);
+    console.log(this.state && this.state.requests);
     temp.forEach(request => {
       allrequests.push(
         <div key={request._id}>
@@ -81,9 +71,23 @@ class Bookings extends Component {
     });
     return <SeeAll items={allrequests} count={50} name="requests" />;
   }
+
+  getCalender() {
+    const MyCalendar = props => (
+      <div>
+        <Calendar
+          localizer={localizer}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500, width: 900 }}
+          events={this.state && this.state.events}
+        />
+      </div>
+    );
+    return <MyCalendar />;
+  }
   render() {
     console.log(this.state.events);
-    console.log(this.state.requests);
     return (
       <Container>
         <Row>
@@ -101,7 +105,7 @@ class Bookings extends Component {
         <br />
         <Row>
           <Col>
-            <MyCalendar />
+            <div className="m:2">{this.getCalender()}</div>
           </Col>
           <Col>
             <h1>New Booking</h1>
