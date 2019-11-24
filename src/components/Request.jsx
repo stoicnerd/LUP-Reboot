d126ea2af8ce9f4f884b0959ccc87ffba2e69ad8
Niral Khambhati <32835426+stoicnerd@users.noreply.github.com>
2019-11-25 02:36:35 +0530

    redesign student dashboard

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -6,6 +6,7 @@ import Card from "react-bootstrap/Card";
 import { getDecodedToken } from "../utils/jwt";
 import { axiosGET, axiosPOST } from "../utils/axiosClient";
 import Button from "react-bootstrap/Button";
+import moment from "moment";
 
 class Request extends Component {
   constructor(props) {
@@ -32,31 +33,55 @@ class Request extends Component {
     let isAdmin = getDecodedToken().role === "notAdmin";
     if (!isAdmin) return <div />;
     if (!this.state.request) return <div />;
+    let stime = new moment(this.state.request.startTime);
+    let etime = new moment(this.state.request.endTime);
     return (
       <Container>
-        <Row>
+        <Row style={{ padding: "5px" }}>
           <Card style={{ width: "72rem" }}>
-            <Card>
-              <Row className="m-1" style={{ fontSize: "20px" }}>
-                Name: {this.state && this.state.request.name}
+            {/* <Row className="m-1">Date: {stime.format('Do MMM YYYY')}</Row>
+              <Row className="m-1">
+                startTime: {stime.format('h:mm A')}
               </Row>
               <Row className="m-1">
-                Email: {this.state && this.state.request.email}
-              </Row>
-              <Row className="m-1">
-                startTime: {this.state && this.state.request.startTime}
-              </Row>
-              <Row className="m-1">
-                endTime: {this.state && this.state.request.endTime}
+                endTime: {etime.format('h:mm A')}
               </Row>
               <Row className="m-1">
                 description: {this.state && this.state.request.description}
               </Row>
               <Row className="m-1">
                 status: {this.state && this.state.request.status}
-              </Row>
-            </Card>
-            {this.state.request.status === "Approved" ? (
+              </Row> */}
+            <Row>
+              <Col>
+                <Row className="m-1">Date: {stime.format("Do MMM YYYY")}</Row>
+                <Row>
+                  <Col className="m-1">From: {stime.format("h:mm A")}</Col>
+                  <Col className="m-1">To: {etime.format("h:mm A")}</Col>
+                </Row>
+              </Col>
+              <Col className="m-1">
+                status: {this.state && this.state.request.status}
+              </Col>
+            </Row>
+            <Row>
+              <Col className="m-1 col-9">
+                description: {this.state && this.state.request.description}
+              </Col>
+              <Col style={{ marginRight: "1rem", marginBottom: "0.5rem" }}>
+                {this.state.request.status === "Approved" ? (
+                  <Button className="m-1" onClick={cancelBooking}>
+                    Cancel Booking
+                  </Button>
+                ) : this.state.request.status === "Requested" ? (
+                  <Button className="m-1" onClick={cancelBooking} block>
+                    Cancel Request
+                  </Button>
+                ) : null}
+              </Col>
+            </Row>
+          </Card>
+          {/* {this.state.request.status === "Approved" ? (
               <Button className="m-1" onClick={cancelBooking}>
                 Cancel Booking
               </Button>
@@ -64,11 +89,10 @@ class Request extends Component {
               <Button className="m-1" onClick={cancelBooking}>
                 Cancel Request
               </Button>
-            ) : null}
-            {/* <Button className="m-1" onClick={cancelBooking}>
+            ) : null} */}
+          {/* <Button className="m-1" onClick={cancelBooking}>
               Cancel Booking
             </Button> */}
-          </Card>
         </Row>
       </Container>
     );
--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -9,6 +9,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Image from "react-bootstrap/Image";
+import cal from "../assets/calendar.png";
+import install from "../assets/install.png";
 
 class StudentDashboard extends Component {
   constructor(props) {
@@ -20,26 +24,54 @@ class StudentDashboard extends Component {
     return (
       <Container>
         <LazyHero>
-          <div>
-            <h1>THIS IS THE STUDENT DASHBOARD!</h1>
+          <div
+            style={{
+              color: "#404040",
+              marginTop: "9rem",
+              marginBottom: "4rem"
+            }}
+          >
+            <h1>What would you like to do?</h1>
           </div>
-          <Card bg="secondary">
-            <Row>
-              <Col>
-                {" "}
-                <Link to={`/booking`}>
-                  <Button>Lab Bookings</Button>
-                </Link>
+          {/* <Card bg="secondary"> */}
+          <Row style={{ alignItems: "center" }}>
+            <ButtonGroup>
+              <Col md={6}>
+                <Button variant="light" size="lg" href="/booking">
+                  <Col xs={6} md={6} style={{ marginLeft: "25%" }}>
+                    <Image src={cal} roundedCircle fluid />
+                  </Col>
+                  <h2>Lab Bookings</h2>
+                  <h6>Book lab for a certain period of time</h6>
+                </Button>
               </Col>
-              <Col>
-                {" "}
-                <Link to={`/installrequest`}>
-                  <Button>Software Installation Requests</Button>
-                </Link>
+              <Col md={6}>
+                <Button variant="light" size="lg" href="/installrequest">
+                  <Col xs={6} md={6} style={{ marginLeft: "25%" }}>
+                    <Image src={install} roundedCircle fluid />
+                  </Col>
+                  <h2>Software Installation Requests</h2>
+                  <h6>Request new/updated software</h6>
+                </Button>
               </Col>
-            </Row>
-          </Card>
+            </ButtonGroup>
+          </Row>
+          {/* </Card> */}
         </LazyHero>
+        <div
+          style={{
+            position: "fixed",
+            left: "0",
+            bottom: "10px",
+            width: "100%",
+            textAlign: "center"
+          }}
+        >
+          <span class="glyphicon glyphicon-calendar" />
+          <span style={{ color: "darkgrey" }}>
+            CSIS Labs, BITS Pilani Hyderabad Campus
+          </span>
+        </div>
       </Container>
     );
   }
