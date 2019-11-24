01769ebfe97c02f346a342fb8ba298df2964ea6d
Niral Khambhati <32835426+stoicnerd@users.noreply.github.com>
2019-11-24 20:34:51 +0530

    softwareRequest functionally working (#42)

--- src/components/SoftwareRequestPage.jsx
+++ src/components/SoftwareRequestPage.jsx
@@ -2,15 +2,45 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { getDecodedToken } from "../utils/jwt";
+import { Card } from "@material-ui/core";
+import { axiosPOST } from "../utils/axiosClient";
 
 class SoftwareRequestPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      softName: "",
+      softLink: "",
+      desc: "",
+      data: {}
+    };
   }
   componentDidMount() {
     console.log("WIP REACHED");
+    let da = getDecodedToken();
+    this.setState({ data: da });
+    console.log(da);
+    console.log(this.state.data);
   }
   render() {
+    let sendSoftReq = () => {
+      let d = getDecodedToken();
+      let reqData = {
+        requesterName: d.name,
+        requesterEmail: d.email,
+        softwareName: this.state && this.state.softName,
+        softwareLink: this.state && this.state.softLink,
+        status: "Requested",
+        description: this.state && this.state.desc
+      };
+      axiosPOST("/api/softwareRequests/", reqData).then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+    };
     return (
       <Container>
         <Row>
@@ -20,11 +50,64 @@ class SoftwareRequestPage extends Component {
             </h1>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <h4>Coming Soon</h4>
-          </Col>
-        </Row>
+        <Card>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Group as={Row} controlId="formPlaintextEmail">
+                  <Form.Label column sm="2">
+                    Request from
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      plaintext
+                      readOnly
+                      defaultValue={this.state.data.name}
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group controlId="formSoftwareName">
+                  <Form.Label>Software Name</Form.Label>
+                  <Form.Control
+                    onChange={event => {
+                      this.setState({ softName: event.target.value });
+                    }}
+                    placeholder="Enter Software Name"
+                  />
+                  <Form.Text className="text-muted">
+                    Case-sensitive in case of clashes
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formSoftwareLink">
+                  <Form.Label>Software Link/Homepage</Form.Label>
+                  <Form.Control
+                    onChange={event => {
+                      this.setState({ softLink: event.target.value });
+                    }}
+                    placeholder="Enter Software Link"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formSoftwareDesc">
+                  <Form.Label>Reason for Request</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    onChange={event => {
+                      this.setState({ desc: event.target.value });
+                    }}
+                    placeholder="For what purpose do you want this software?"
+                  />
+                </Form.Group>
+              </Form>
+              <Button
+                style={{ margin: "15px" }}
+                variant="secondary"
+                onClick={sendSoftReq}
+              >
+                Request Booking
+              </Button>
+            </Col>
+          </Row>
+        </Card>
       </Container>
     );
   }
