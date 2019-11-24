1cdd321a118415a567bea72d68d199c4ba9965f9
Niral Khambhati <32835426+stoicnerd@users.noreply.github.com>
2019-11-25 03:08:57 +0530

    redesign adminDashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -5,6 +5,11 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import cal from "../assets/calendar.png";
+import install from "../assets/install.png";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import LazyHero from "react-lazy-hero";
 
 class AdminDashboard extends Component {
   constructor(props) {
@@ -17,7 +22,56 @@ class AdminDashboard extends Component {
   render() {
     return (
       <Container>
-        <Row>
+        <LazyHero>
+          <div
+            style={{
+              color: "#404040",
+              marginTop: "9rem",
+              marginBottom: "4rem"
+            }}
+          >
+            <h1>Hello, Admin!</h1>
+          </div>
+          {/* <Card bg="secondary"> */}
+          <Row style={{ alignItems: "center" }}>
+            <ButtonGroup>
+              <Col md={6}>
+                <Button variant="light" size="lg" href="/admin/booking">
+                  <Col xs={6} md={6} style={{ marginLeft: "25%" }}>
+                    <Image src={cal} roundedCircle fluid />
+                  </Col>
+                  <h2>Lab Bookings Requests</h2>
+                  <h6>Approve/Reject Lab Booking Requests</h6>
+                </Button>
+              </Col>
+              <Col md={6}>
+                <Button variant="light" size="lg" href="/admin/installrequest">
+                  <Col xs={6} md={6} style={{ marginLeft: "25%" }}>
+                    <Image src={install} roundedCircle fluid />
+                  </Col>
+                  <h2>Software Installation Requests</h2>
+                  <h6>Approve/Reject Installation Requests</h6>
+                </Button>
+              </Col>
+            </ButtonGroup>
+          </Row>
+          {/* </Card> */}
+        </LazyHero>
+        <div
+          style={{
+            position: "fixed",
+            left: "0",
+            bottom: "10px",
+            width: "100%",
+            textAlign: "center"
+          }}
+        >
+          <span class="glyphicon glyphicon-calendar" />
+          <span style={{ color: "darkgrey" }}>
+            CSIS Labs, BITS Pilani Hyderabad Campus
+          </span>
+        </div>
+        {/* <Row>
           <Col>
             <h1 className="text-center">THIS IS THE GREAT ADMIN'S DASHBOARD</h1>
           </Col>
@@ -43,7 +97,7 @@ class AdminDashboard extends Component {
               </Link>
             </Col>
           </Row>
-        </Card>
+        </Card> */}
       </Container>
     );
   }
